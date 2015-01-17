f80eda9f713b99e57d5683249f2555181812228a
Aaron Barber <asbarber@umich.edu>
2015-01-17 17:31:29 -0500

    Trying to fix the venmo magic oauth

--- android/MHacksV/app/src/main/java/com/example/mray/venmo/VenmoActivity.java
+++ android/MHacksV/app/src/main/java/com/example/mray/venmo/VenmoActivity.java
@@ -5,18 +5,36 @@ import android.net.Uri;
 
 import com.example.mray.mhacksv.MyoListener;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.IOException;
+import java.net.URL;
+
 /**
  * Created by Aaron Barber on 17/01/15.
  */
 public class VenmoActivity {
 
-    public Intent transfer(String payer, String payee, double amount){
+    public void transfer(String payer, String payee, double amount){
         String venmo_uri = "https://api.venmo.com/v1/payments" +
                 "?access_token=" + payer +
                 "&user_id=" + payee +
                 "&amount=" + Double.toString(-1*Math.abs(amount)) +
                 "&note=" + "MHacks";
 
-        return new Intent(Intent.ACTION_VIEW, Uri.parse(venmo_uri));
+
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost post = new HttpPost(venmo_uri);
+        try {
+            HttpResponse response = httpclient.execute(post);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
