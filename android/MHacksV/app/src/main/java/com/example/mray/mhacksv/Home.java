3a3c6269b3e0ab196e98e0b480c74552c63c41c8
Michael Ray <mray190@gmail.com>
2015-01-17 03:35:05 -0500

    Implemented Myo SDK and detects handshake

--- android/MHacksV/app/src/main/java/com/example/mray/mhacksv/Home.java
+++ android/MHacksV/app/src/main/java/com/example/mray/mhacksv/Home.java
@@ -1,39 +1,80 @@
 package com.example.mray.mhacksv;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
+import com.thalmic.myo.Hub;
 
 public class Home extends ActionBarActivity {
+    private Hub hub;
+    private MyoListener myoListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        initHub();
+        setupEventListener();
     }
 
+    private void initHub() {
+        TextView hub_status = (TextView)findViewById(R.id.hub_status);
+        hub = Hub.getInstance();
+        if (!hub.init(this)) {
+            hub_status.setText("Hub could not be initialized");
+            finish();
+            return;
+        }
+        hub_status.setText("Hub initialized");
+        hub.setLockingPolicy(Hub.LockingPolicy.NONE);
+        return;
+    }
+
+    public void connect1(View view) {
+        for (int i=0; i<hub.getConnectedDevices().size(); i++) {
+            hub.detach(hub.getConnectedDevices().get(i).getMacAddress());
+        }
+        hub.attachByMacAddress("DF:34:F2:52:47:20");
+    }
+
+    public void connect2(View view) {
+        for (int i=0; i<hub.getConnectedDevices().size(); i++) {
+            hub.detach(hub.getConnectedDevices().get(i).getMacAddress());
+        }
+        hub.attachByMacAddress("E6:35:8E:89:45:58");
+    }
+
+    private void setupEventListener() {
+        myoListener = new MyoListener(this);
+        hub.addListener(myoListener);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStop() {
+        hub.removeListener(myoListener);
+        super.onStop();
     }
 }
