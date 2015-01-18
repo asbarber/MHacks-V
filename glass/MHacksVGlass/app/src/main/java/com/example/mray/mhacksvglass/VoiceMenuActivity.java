59088a0c12e355c9b8064149d7c40dd2b6ed32ab
sjacker <sjacker@umich.edu>
2015-01-17 21:59:02 -0500

    glass clean up, everything commented out but not deleted

--- glass/MHacksVGlass/app/src/main/java/com/example/mray/mhacksvglass/VoiceMenuActivity.java
+++ glass/MHacksVGlass/app/src/main/java/com/example/mray/mhacksvglass/VoiceMenuActivity.java
@@ -1,52 +1,52 @@
-package com.example.mray.mhacksvglass;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.View;
-import android.view.WindowManager;
-
-import com.google.android.glass.view.WindowUtils;
-
-/**
- * An activity that demonstrates the voice menu API.
- */
-public final class VoiceMenuActivity extends Activity {
-
-    private int mPicture = 0;
-    private boolean mVoiceMenuEnabled = true;
-
-    @Override
-    protected void onCreate(Bundle bundle) {
-        super.onCreate(bundle);
-
-        // Requests a voice menu on this activity. As for any other window feature,
-        // be sure to request this before setContentView() is called
-        getWindow().requestFeature(WindowUtils.FEATURE_VOICE_COMMANDS);
-
-        // Ensure screen stays on during demo.
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
-    }
-
-    @Override
-    public boolean onCreatePanelMenu(int featureId, Menu menu) {
-        if (featureId == WindowUtils.FEATURE_VOICE_COMMANDS) {
-            getMenuInflater().inflate(R.menu.voice_menu, menu);
-            return true;
-        }
-        // Good practice to pass through, for options menu.
-        return super.onCreatePanelMenu(featureId, menu);
-    }
-
-    @Override
-    public boolean onPreparePanel(int featureId, View view, Menu menu) {
-        if (featureId == WindowUtils.FEATURE_VOICE_COMMANDS) {
-            // Dynamically decides between enabling/disabling voice menu.
-            return mVoiceMenuEnabled;
-        }
-        // Good practice to pass through, for options menu.
-        return super.onPreparePanel(featureId, view, menu);
-    }
-
-}
+//package com.example.mray.mhacksvglass;
+//
+//import android.app.Activity;
+//import android.os.Bundle;
+//import android.view.Menu;
+//import android.view.View;
+//import android.view.WindowManager;
+//
+//import com.google.android.glass.view.WindowUtils;
+//
+///**
+//* An activity that demonstrates the voice menu API.
+//*/
+//public final class VoiceMenuActivity extends Activity {
+//
+//    private int mPicture = 0;
+//    private boolean mVoiceMenuEnabled = true;
+//
+//    @Override
+//    protected void onCreate(Bundle bundle) {
+//        super.onCreate(bundle);
+//
+//        // Requests a voice menu on this activity. As for any other window feature,
+//        // be sure to request this before setContentView() is called
+//        getWindow().requestFeature(WindowUtils.FEATURE_VOICE_COMMANDS);
+//
+//        // Ensure screen stays on during demo.
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+//
+//    }
+//
+//    @Override
+//    public boolean onCreatePanelMenu(int featureId, Menu menu) {
+//        if (featureId == WindowUtils.FEATURE_VOICE_COMMANDS) {
+//            getMenuInflater().inflate(R.menu.voice_menu, menu);
+//            return true;
+//        }
+//        // Good practice to pass through, for options menu.
+//        return super.onCreatePanelMenu(featureId, menu);
+//    }
+//
+//    @Override
+//    public boolean onPreparePanel(int featureId, View view, Menu menu) {
+//        if (featureId == WindowUtils.FEATURE_VOICE_COMMANDS) {
+//            // Dynamically decides between enabling/disabling voice menu.
+//            return mVoiceMenuEnabled;
+//        }
+//        // Good practice to pass through, for options menu.
+//        return super.onPreparePanel(featureId, view, menu);
+//    }
+//}
+//
